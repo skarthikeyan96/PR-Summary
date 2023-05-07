f300a91396e7aae4b79bab352e6dec5c89a9e871
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2023-05-07 16:22:36 +0530

    prod dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,77 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { Octokit } from "@octokit/core";
+import { OpenAIApi, Configuration } from "openai";
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    if (github.context.eventName !== "pull_request") {
+      // The core module on the other hand let's you get
+      // inputs or create outputs or control the action flow
+      // e.g. by producing a fatal error
+      core.setFailed("Can only run on pull requests!");
+      return;
+    }
+  
+    const github_token = core.getInput("GITHUB_TOKEN");
+    const openai_key = core.getInput("OPENAI_API_KEY");
+  
+    const octokit = new Octokit({
+      auth: github_token,
+    });
+  
+    const configuration = new Configuration({
+      apiKey: openai_key,
+    });
+  
+    const openai = new OpenAIApi(configuration);
+  
+    // get the pr details
+  
+    const { repo, owner, number } = github.context.issue;
+    try {
+      const response = await octokit.request(
+        "GET /repos/{owner}/{repo}/pulls/{pull_number}",
+        {
+          owner,
+          repo,
+          pull_number: number,
+          headers: {
+            "X-GitHub-Api-Version": "2022-11-28",
+          },
+        }
+      );
+  
+      const { title, body, patch_url } = response.data;
+  
+      const summaryResponse = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `Pull Request Summary: Title: ${title} Description: ${
+          body || ""
+        } Diff: ${patch_url}`,
+        temperature: 0.7,
+        max_tokens: 100,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
+  
+      await octokit.request(
+        "POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
+        {
+          owner,
+          repo,
+          issue_number: number,
+          body: summaryResponse.data.choices[0].text || "",
+          headers: {
+            "X-GitHub-Api-Version": "2022-11-28",
+          },
+        }
+      );
+    } catch (error) {
+      console.log(error);
+      core.setFailed("something went wrong");
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
